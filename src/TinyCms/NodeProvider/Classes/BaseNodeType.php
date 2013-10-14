98392186cc3b2a229459505789305a939ba24188
Wilfried Reiter <wilfried.reiter@devpoint.at>
2013-10-14 10:46:36 +0200

    removed constructor

--- src/TinyCms/NodeProvider/Classes/BaseNodeType.php
+++ src/TinyCms/NodeProvider/Classes/BaseNodeType.php
@@ -4,16 +4,7 @@ namespace TinyCms\NodeProvider\Classes;
 
 abstract class BaseNodeType extends BaseEntityType {
 
-	/*
-	 * Constructor
-	 *
-	 * @param $typeName string
-	 */
-	protected function __construct($typeName, $parentType, $description)
-	{
-		parent::__construct($typeName, $parentType, $description);
-	}
-
+	
 
 }
 
